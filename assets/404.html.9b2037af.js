807040b6ac7e8a901801172115512a054b586e79
swappet <swap.pet@protonmail.com>
2022-04-06 21:51:54 +0800

    deploy web : V0.01:init

---
+++ assets/404.html.9b2037af.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app.9d48f95c.js";const _={};function e(t,c){return null}var a=r(_,[["render",e]]);export{a as default};
---
+++ assets/welcome.html.991edd35.js
@@ -0,0 +1,15 @@
+const e={key:"v-1abeb830",path:"/welcome.html",title:"Welcome to Swap.Pet",lang:"en-US",frontmatter:{index:1,title:"Welcome to Swap.Pet",author:"Swap.Pet",date:"2022-03-28T00:00:00.000Z",category:["Guide"],tag:["Welcome"],sticky:!0,star:!0,summary:`Swap Pet : Swap any pet.
+You can support us at:
+
+gitcoin.co :
+dorahacks.io :
+devpost.com :
+
+`,head:[["meta",{property:"og:url",content:"https://swap.pet/welcome.html"}],["meta",{property:"og:title",content:"Welcome to Swap.Pet"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{property:"article:author",content:"Swap.Pet"}],["meta",{property:"article:tag",content:"Welcome"}],["meta",{property:"article:published_time",content:"2022-03-28T00:00:00.000Z"}]]},excerpt:`<p>Swap Pet : Swap any pet.</p>
+<p>You can support us at:</p>
+<ol>
+<li>gitcoin.co :</li>
+<li>dorahacks.io :</li>
+<li>devpost.com :</li>
+</ol>
+`,headers:[{level:2,title:"Swap Pet",slug:"swap-pet",children:[]},{level:2,title:"Syber Connect",slug:"syber-connect",children:[]},{level:2,title:"Why SwapPet?",slug:"why-swappet",children:[]},{level:2,title:"Tech Stack to Build Swap Pet",slug:"tech-stack-to-build-swap-pet",children:[{level:3,title:"Identity Management",slug:"identity-management",children:[]},{level:3,title:"Account System",slug:"account-system",children:[]},{level:3,title:"Decentralized Networks",slug:"decentralized-networks",children:[]}]}],git:{createdTime:1649253083e3,updatedTime:1649253083e3,contributors:[{name:"swappet",email:"swap.pet@protonmail.com",commits:1}]},readingTime:{minutes:.44,words:133},filePathRelative:"welcome.md"};export{e as data};
